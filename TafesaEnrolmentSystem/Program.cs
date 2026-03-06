4bcd9e943e828faafa7dac7b27bffbf716b95e5a
Craig Arnold <craig.arnold@student.tafe.edu.au>
2026-03-06 12:07:00 +1100

    Program.cs: Add testing of all standard methods and constructors for all classes.

--- TafesaEnrolmentSystem/Program.cs
+++ TafesaEnrolmentSystem/Program.cs
@@ -11,7 +11,116 @@ namespace TafesaEnrolmentSystem
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("TEST ADDRESS CLASS");
+            Console.WriteLine("");
+            Console.WriteLine("Test no-arg constructor (address1):");
+            Address address1 = new Address();
+            Console.WriteLine(address1);
+            Console.WriteLine("");
+            Console.WriteLine("Test all-arg constructor (address2):");
+            Address address2 = new Address("100", "Grenfell St", "Adelaide", "5000", "SA");
+            Console.WriteLine(address2);
+            Console.WriteLine("");
+            Console.WriteLine("Test setters/getters (set new values for address1):");
+            address1.StreetNum = "50";
+            address1.StreetName= "Currie St";
+            address1.Suburb = "Adelaide";
+            address1.State = "SA";
+            address1.Postcode = "5007";
+            Console.WriteLine(address1);
+            Console.WriteLine("(print postcode with getter): " + address1.Postcode);
+            Console.WriteLine("");
+
+            Console.WriteLine("----------------------------"); 
+            Console.WriteLine("TEST SUBJECT CLASS:");
+            Console.WriteLine("");
+            Console.WriteLine("Test no-arg constructor (subject1)");
+            Subject subject1 = new Subject();
+            Console.WriteLine(subject1);
+            Console.WriteLine("");
+            Console.WriteLine("Test all-arg constructor (subject2): ");
+            Subject subject2 = new Subject("IT_PHP02", "PHP", 570);
+            Console.WriteLine(subject2);
+            Console.WriteLine("");
+            Console.WriteLine("Test setters/getters (change details of subject1):");
+            subject1.Cost = 800;
+            subject1.SubjectName = "JAVA";
+            subject1.SubjectCode = "IT_JAVA02";
+            Console.WriteLine(subject1);
+            Console.WriteLine("(print subject code with getter): " + subject1.SubjectCode);
+            Console.WriteLine("");
+
+            Console.WriteLine("----------------------------"); 
+            Console.WriteLine("TEST ENROLLMENT CLASS:");
+            Console.WriteLine("");
+            Console.WriteLine("Test no-arg constructor(enrollment1)");
+            Enrollment enrollment1 = new Enrollment();
+            Console.WriteLine(enrollment1);
+            Console.WriteLine("");
+            Console.WriteLine("Test all-arg constructor (enrollment2):");
+            Enrollment enrollment2 = new Enrollment(new DateTime(2026, 2, 28), "PA", 1, subject2);
+            Console.WriteLine(enrollment2);
+            Console.WriteLine("");
+            Console.WriteLine("Test setters/getters (set subject of enrollment1:");
+            enrollment1.Subject = subject1;
+            Console.WriteLine(enrollment1);
+            Console.WriteLine("Print subject using getter: " + enrollment1.Subject);
+            Console.WriteLine("");
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("TEST PERSON CLASS:");
+            Console.WriteLine("");
+            Console.WriteLine("Test no-arg constructor (person1)");
+            Person person1 = new Person();
+            Console.WriteLine(person1);
+            Console.WriteLine("");
+            Console.WriteLine("Test all-arg constructor (person2)");
+            Person person2 = new Person("Frank Smith", "fsmith@gmail.com", "0400 666 777", address2);
+            Console.WriteLine(person2);
+            Console.WriteLine("");
+            Console.WriteLine("Test setters/getters (change details of person1):");
+            person1.Name = "Danny Smithson";
+            person1.Email = "DannyS@gmail.com";
+            person1.PhoneNumber = "0411 222 333";
+            person1.Address = address1;
+            Console.WriteLine(person1);
+            Console.WriteLine("print person1 email using getter: " + person1.Email);
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("TEST STUDENT CLASS:");
+            Console.WriteLine("");
+            Console.WriteLine("Test no-arg constructor( student1):");
+            Student student1 = new Student();
+            Console.WriteLine(student1);
+            Console.WriteLine("");
+            Console.WriteLine("Test all-arg constructor [including adding two enrollments] (student2):");
+            Student student2 = new Student("100600", "DiplomaIT", DateTime.Now, "Craig A", "craiga@gmail.com", "0444 555 999", address1);
+            student2.Enrollments.Add(enrollment1);
+            student2.Enrollments.Add(new Enrollment(DateTime.Now, "Not Graded", 1, subject2));
+            Console.WriteLine(student2);
+            Console.WriteLine("");
+            Console.WriteLine("Test StudentID only constructor (student3):");
+            Student student3 = new Student("100650");
+            Console.WriteLine(student3);
+            Console.WriteLine("");
+
+            Console.WriteLine("Test setters/getters (change student1 details):");
+            student1.StudentID = "999888";
+            student1.Name = "Craig B";
+            student1.Email = "craig_b@gmail.com";
+            student1.PhoneNumber = "0449 000 111";
+            student1.Address.StreetNum = "10";
+            student1.Address.StreetName = "Rundle Street";
+            student1.Address.Suburb = "Adelaide";
+            student1.Address.Postcode = "5000";
+            student1.Address.State = "SA";
+            student1.Program = "Diploma IT";
+            Console.WriteLine(student1);
+            Console.WriteLine("Print studentID using getter: " + student1.StudentID);
+
+
+
         }
     }
 }
